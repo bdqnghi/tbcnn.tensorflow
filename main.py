39d3c24936446232e505fb7e3bfee8724438dfab
Quoc Nghi <bdqnghi@gmail.com>
2018-11-24 19:13:38 +0800

    update

--- main.py
+++ main.py
@@ -34,9 +34,9 @@ parser.add_argument('--cuda', action='store_true', help='enables cuda')
 parser.add_argument('--verbal', type=bool, default=True, help='print training info or not')
 parser.add_argument('--manualSeed', type=int, help='manual seed')
 parser.add_argument('--n_classes', type=int, default=3, help='manual seed')
-parser.add_argument('--train_directory', default="ProgramData_pkl_train_test_val/train", help='train program data')
-parser.add_argument('--test_directory', default="ProgramData_pkl_train_test_val/test", help='test program data')
-parser.add_argument('--val_directory', default="ProgramData_pkl_train_test_val/val", help='validation program data')
+parser.add_argument('--train_directory', default="github_cpp_pkl_train_test_val/train", help='train program data')
+parser.add_argument('--test_directory', default="github_cpp_pkl_train_test_val/test", help='test program data')
+parser.add_argument('--val_directory', default="github_cpp_pkl_train_test_val/val", help='validation program data')
 parser.add_argument('--model_path', default="model/3_pku", help='path to save the model')
 parser.add_argument('--n_hidden', type=int, default=50, help='number of hidden layers')
 parser.add_argument('--training', action="store_true",help='is training')
@@ -49,9 +49,11 @@ parser.add_argument('--aggregation', type=int, default=1, help='0 for max poolin
 parser.add_argument('--embeddings_directory', default="embedding/fast_pretrained_vectors.pkl", help='pretrained embeddings url, there are 2 objects in this file, the first object is the embedding matrix, the other is the lookup dictionary')
 
 opt = parser.parse_args()
+if not os.path.isdir("cached"):
+    os.mkdir("cached")
 
 # def train_model(opt):
-def train_model(train_trees, test_trees, val_trees, labels, embeddings, embedding_lookup, opt):
+def train_model(train_trees, val_trees, labels, embeddings, embedding_lookup, opt):
     """Train a classifier to label ASTs"""
    
     logdir = opt.model_path
@@ -61,7 +63,7 @@ def train_model(train_trees, test_trees, val_trees, labels, embeddings, embeddin
     
     random.shuffle(train_trees)
     random.shuffle(val_trees)
-    random.shuffle(test_trees)
+    # random.shuffle(test_trees)
     
     checkfile = os.path.join(logdir, 'cnn_tree.ckpt')   
     ckpt = tf.train.get_checkpoint_state(logdir)
@@ -172,16 +174,7 @@ def train_model(train_trees, test_trees, val_trees, labels, embeddings, embeddin
 
             print("Finish all iters, storring the whole model..........")
             saver.save(sess, checkfile)
-            # builder = saved_model.builder.SavedModelBuilder(savedmodel_path)
-            # signature = predict_signature_def(inputs={'nodes': nodes_node, "children": children_node},
-            #                                   outputs={'labels': labels_node})
-            # # using custom tag instead of: tags=[tag_constants.SERVING]
-            # builder.add_meta_graph_and_variables(sess=sess,
-            #                                      tags=[tag_constants.TRAINING],
-            #                                      signature_def_map={'predict': signature})
-            # builder.save()
-            # saved_model.simple_save(sess,savedmodel_path,inputs={'nodes': nodes_node, "children": children_node},
-                                              # outputs={'labels': labels_node})
+        
 
     if opt.testing:
         with tf.Session() as sess:
@@ -218,60 +211,84 @@ def train_model(train_trees, test_trees, val_trees, labels, embeddings, embeddin
             print(classification_report(correct_labels, predictions, target_names=target_names))
             print(confusion_matrix(correct_labels, predictions))
 
-# def test_model(trees, labels, embeddings, embedding_lookup, opt):
+def test_model(test_trees, labels, embeddings, embedding_lookup, opt):
+    logdir = opt.model_path
+    batch_size = opt.train_batch_size
+    epochs = opt.niter
+    node_embedding_size = len(embeddings[0])
     
-#     logdir = opt.model_path
-#     batch_size = opt.train_batch_size
-#     epochs = opt.niter
-#     num_feats = len(embeddings[0])
+    random.shuffle(test_trees)
+    
+    checkfile = os.path.join(logdir, 'cnn_tree.ckpt')   
+    ckpt = tf.train.get_checkpoint_state(logdir)
+    
+    std = 1.0 / math.sqrt(node_embedding_size)
+    weights = {
+        "w_t" : tf.Variable(tf.truncated_normal([node_embedding_size, opt.feature_size], stddev=std), name="w_t"),
+        "w_l" : tf.Variable(tf.truncated_normal([node_embedding_size, opt.feature_size], stddev=std), name="w_l"),
+        "w_r" : tf.Variable(tf.truncated_normal([node_embedding_size, opt.feature_size], stddev=std), name="w_r"),
+        "w_attention" : tf.Variable(tf.truncated_normal([opt.feature_size,1], stddev=std), name="w_attention"),
+        "w_hidden": tf.Variable(tf.truncated_normal([opt.feature_size, len(labels)], stddev=1.0 / math.sqrt(opt.feature_size)),name="w_hidden")
+    }
 
-#     random.shuffle(trees)
-#     # build the inputs and outputs of the network
-#     nodes_node, children_node, hidden_node, pooling = network.init_net(
-#         num_feats,
-#         len(labels),
-#         opt.aggregation
-#     )
+    biases = {
+        "b_conv": tf.Variable(tf.truncated_normal([opt.feature_size,], stddev=math.sqrt(2.0/node_embedding_size)), name="b_conv"),
+        "b_hidden": tf.Variable(tf.truncated_normal([len(labels),], stddev=math.sqrt(2.0/node_embedding_size)), name="b_hidden")
+    }
 
-#     out_node = network.out_layer(hidden_node)
-#     labels_node, loss_node = network.loss_layer(hidden_node, len(labels))
+    nodes_node, children_node, hidden_node, attention_score_node = network.init_net(
+        node_embedding_size,
+        len(labels),
+        opt.feature_size,
+        weights,
+        biases,
+        opt.aggregation
+    )
+    hidden_node = tf.identity(hidden_node, name="hidden_node")
 
-#     optimizer = tf.train.AdamOptimizer(LEARN_RATE)
-#     train_step = optimizer.minimize(loss_node)
+    out_node = network.out_layer(hidden_node)
+    labels_node, loss_node = network.loss_layer(hidden_node, len(labels))
 
-#     sess = tf.Session()#config=tf.ConfigProto(device_count={'GPU':0}))
-#     sess.run(tf.global_variables_initializer())
-#     with tf.name_scope('saver'):
-#         saver = tf.train.Saver()
-#         ckpt = tf.train.get_checkpoint_state(logdir)
-#         if ckpt and ckpt.model_checkpoint_path:
-#             print("Continue training with old model")
-#             saver.restore(sess, ckpt.model_checkpoint_path)
+    optimizer = tf.train.AdamOptimizer(LEARN_RATE)
+    train_step = optimizer.minimize(loss_node)
+    
+    saver = tf.train.Saver(save_relative_paths=True, max_to_keep=5)
+    # Initialize the variables (i.e. assign their default value)
+    init = tf.global_variables_initializer()
 
-#     checkfile = os.path.join(logdir, 'cnn_tree.ckpt')
+    with tf.Session() as sess:
+        sess.run(init)
+        ckpt = tf.train.get_checkpoint_state(logdir)
+        if ckpt and ckpt.model_checkpoint_path:
+            print("Continue training with old model")
+            print("Checkpoint path : " + str(ckpt.model_checkpoint_path))
+            saver.restore(sess, ckpt.model_checkpoint_path)
 
-#     correct_labels = []
-#     predictions = []
-#     print('Computing training accuracy...')
-#     for batch in sampling.batch_samples(
-#         sampling.gen_samples(trees, labels, embeddings, embedding_lookup), 1
-#     ):
-#         nodes, children, batch_labels = batch
-#         output = sess.run([out_node],
-#             feed_dict={
-#                 nodes_node: nodes,
-#                 children_node: children,
-#             }
-#         )
-#         # print(output)
-#         correct_labels.append(np.argmax(batch_labels))
-#         predictions.append(np.argmax(output))
+        correct_labels = []
+        predictions = []
+        print('Computing training accuracy...')
+        for batch in sampling.batch_samples(
+            sampling.gen_samples(test_trees, labels, embeddings, embedding_lookup), 1
+        ):
+            nodes, children, batch_labels = batch
+            output, attention_score = sess.run([out_node, attention_score_node],
+                feed_dict={
+                    nodes_node: nodes,
+                    children_node: children,
+                }
+            )
 
-#     target_names = list(labels)
-#     print('Accuracy:', accuracy_score(correct_labels, predictions))
-#     print(classification_report(correct_labels, predictions, target_names=target_names))
-#     print(confusion_matrix(correct_labels, predictions))
+            # print(attention_score[0])
+            # print(len(attention_score[0]))
+         
+            # print(output)
+            correct_labels.append(np.argmax(batch_labels))
+            predictions.append(np.argmax(output))
 
+        target_names = list(labels)
+        print('Accuracy:', accuracy_score(correct_labels, predictions))
+        print(classification_report(correct_labels, predictions, target_names=target_names))
+        print(confusion_matrix(correct_labels, predictions))
 
 def main(opt):
 
@@ -281,18 +298,27 @@ def main(opt):
        
     # if opt.training:
     labels = [str(i) for i in range(1, opt.n_classes+1)]
-    print("Loading train trees...")
-    train_data_loader = MonoLanguageProgramData(opt.train_directory, 0, opt.n_classes)
-    train_trees, _ = train_data_loader.trees, train_data_loader.labels
   
 
-    val_data_loader = MonoLanguageProgramData(opt.val_directory, 2, opt.n_classes)
-    val_trees, _ = val_data_loader.trees, val_data_loader.labels
+    if opt.training:
+        print("Loading train trees...")
+        train_data_loader = MonoLanguageProgramData(opt.train_directory, 0, opt.n_classes)
+        train_trees, _ = train_data_loader.trees, train_data_loader.labels
+      
 
-    test_data_loader = MonoLanguageProgramData(opt.test_directory, 1, opt.n_classes)
-    test_trees, _ = test_data_loader.trees, test_data_loader.labels
- 
-    train_model(train_trees, test_trees, val_trees, labels, embeddings, embed_lookup , opt) 
+        val_data_loader = MonoLanguageProgramData(opt.val_directory, 2, opt.n_classes)
+        val_trees, _ = val_data_loader.trees, val_data_loader.labels
+
+        train_model(train_trees, val_trees, labels, embeddings, embed_lookup , opt) 
+
+    if opt.testing:
+        print("Loading test trees...")
+        test_data_loader = MonoLanguageProgramData(opt.test_directory, 1, opt.n_classes)
+        test_trees, _ = test_data_loader.trees, test_data_loader.labels
+        
+        test_model(test_trees, labels, embeddings, embed_lookup , opt) 
+
+    
 
     # if opt.testing:
 
