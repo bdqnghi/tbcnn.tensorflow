c2bc69226749ff7ee48636b07e9f8de8898409d6
Yijun Yu <y.yu@open.ac.uk>
2018-11-24 16:06:04 +0000

    update

--- t.sh
+++ t.sh
@@ -1,4 +1,3 @@
-file=${1:-live_test/github_cpp/26/10.cpp}
 if [ ! -f embedding/fast_pretrained_vectors.pkl ]; then
 	pip3 install --upgrade pip
 	pip3 install tensorflow
@@ -8,7 +7,13 @@ if [ ! -f embedding/fast_pretrained_vectors.pkl ]; then
 	tar xvfz embedding/fast_pretrained_vectors.pkl.tar.gz
 	mv fast_pretrained_vectors.pkl embedding/
 fi
-if [ ! -f ${file/.cpp/.pb} ]; then
-	fast -p $file ${file/.cpp/.pb}
+file=${1:-live_test/github_cpp/26/10.cpp}
+f=${file/.cpp/}
+function fast() {
+  docker run --rm -v $(pwd):/e -w /e -it yijun/fast:built $@
+}
+export -f fast
+if [ ! -f $f.pb ]; then
+  fast -p $f.cpp $f.pb
 fi
-python3 live_test.py --model_path model/github_50_cpp_new --n_classes=50 --test_file $file
+python3 live_test.py --model_path model/github_50_cpp_new --n_classes=50 --test_file $f.cpp
