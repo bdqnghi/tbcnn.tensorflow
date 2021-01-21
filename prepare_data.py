3e679cc7eb51437b0665b83a935292055d003492
Quoc Nghi <bdqnghi@gmail.com>
2021-01-21 20:27:37 +0800

    Clean up

--- prepare_data.py
+++
@@ -1,4 +0,0 @@
-import tarfile
-tar = tarfile.open("embedding/fast_pretrained_vectors.pkl.tar.gz")
-tar.extractall("embedding/")
-tar.close()
--- t.sh
+++
@@ -1,8 +0,0 @@
-if [ ! -f embedding/fast_pretrained_vectors.pkl ]; then
-	pip3 install --upgrade pip
-	pip3 install -r requirements.txt
-	tar xvfz embedding/fast_pretrained_vectors.pkl.tar.gz
-	mv fast_pretrained_vectors.pkl embedding/
-fi
-file=${1:-live_test/github_cpp/26/10.cpp}
-python live_test.py --model_path model/github_50_cpp_new --n_classes=50 --test_file $file
