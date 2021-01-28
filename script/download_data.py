52277ee97a4fafa1e141efb7530bfd52f70ec072
Quoc Nghi <quocnghi@Quocs-MacBook-Pro.local>
2021-01-28 20:35:26 +0700

    update download data

--- script/download_data.py
+++ script/download_data.py
@@ -1,7 +1,7 @@
 import urllib.request
 import zipfile
 from tqdm import tqdm
-
+import os
 
 class DownloadProgressBar(tqdm):
     def update_to(self, b=1, bsize=1, tsize=None):
@@ -22,8 +22,11 @@ code_classification_output_path = "../OJ_pycparser_train_test_val.zip"
 pretrained_model_url = "https://ai4code.s3-ap-southeast-1.amazonaws.com/model_OJ_pycparser.zip"
 pretrained_model_output_path = "../model_OJ_pycparser.zip"
 
-download_url(code_classification_data_url, code_classification_output_path)
-download_url(pretrained_model_url, pretrained_model_output_path)
+if not os.path.exists(code_classification_output_path):
+    download_url(code_classification_data_url, code_classification_output_path)
+
+if not os.path.exists(pretrained_model_output_path):
+    download_url(pretrained_model_url, pretrained_model_output_path)
 
 with zipfile.ZipFile(code_classification_output_path) as zf:
     for member in tqdm(zf.infolist(), desc='Extracting '):
